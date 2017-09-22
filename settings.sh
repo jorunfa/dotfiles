ec9f80f599abbe443e66aedeece25c7c1779e883
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2017-09-22 08:37:50 +0200

    Change git name and email, and default brew casks

--- settings.sh
+++ settings.sh
@@ -1,18 +1,18 @@
 # GIT
-GIT_NAME="Morten Noddeland"
-GIT_EMAIL="morten@noddeland.no"
+GIT_NAME="Jørund B. Fagerjord"
+GIT_EMAIL="j.b.fagerjord@gmail.com"
 
 # APPLICATIONS
 # The applications below are regular apps such as Chrome and Dropbox.
 # Please make sure that you remove all the programs you don't want to install on your computer
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
-BREW_CASKS="1password alfred arq atom balsamiq-mockups disk-inventory-x dropbox evernote flux gitx-rowanj google-chrome iconjar imageoptim iterm2 jetbrains-toolbox licecap opera postman postbox skype slack spotify steam sublime-text superduper teamviewer the-unarchiver transmission vlc"
+BREW_CASKS="alfred disk-inventory-x google-chrome iterm2 postman skype slack spotify steam sublime-text vlc dropbox"
 
 # NODE
 NPM_PACKAGES="n ncu yarn"
 
 # HYPER (installed with hpm)
-HPM_PACKAGES="hyper-snazzy hyperlinks hyperterm-1password"
+HPM_PACKAGES="hyper-snazzy hyperlinks"
 
 # BREW (OPTIONAL)
 OPTIONAL_BREW_PACKAGES="pyenv python3 wget"
