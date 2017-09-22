047796e05011accf760fa91ab3b1ed19ba79add6
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2017-09-22 12:46:39 +0200

    Use brew curl instead of system

--- dotfiles/fish/config/config.fish
+++ dotfiles/fish/config/config.fish
@@ -16,3 +16,4 @@ set -gx LESS_TERMCAP_se \e'[0m'           # end standout-mode
 set -gx LESS_TERMCAP_so \e'[38;5;246m'    # begin standout-mode - info box
 set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
 set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
+set -g fish_user_paths "/usr/local/opt/curl/bin" $fish_user_paths
