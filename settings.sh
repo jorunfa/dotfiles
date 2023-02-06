20ad5fa00a869592761d05908cd9988b3976d18c
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2023-02-06 10:49:14 +0100

    make rough update of settings.sh

--- settings.sh
+++ settings.sh
@@ -6,16 +6,19 @@ GIT_EMAIL="j.b.fagerjord@gmail.com"
 # The applications below are regular apps such as Chrome.
 # Please make sure that you remove all the programs you don't want to install on your computer
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
-BREW_CASKS="abstract chromedriver google-chrome keybase sizeup streamlink-twitch-gui visual-studio-code alfred homebrew/cask-versions/google-chrome-canary logitech-gaming-software sketch sublime-text vlc android-studio discord google-drive-file-stream skyfonts supertuxkart zulu android-studio-preview disk-inventory-x hyper obs skype tor-browser zulu8 docker intellij-idea slack aws-vault osu spotify ubiquiti-unifi-controller balsamiq-mockups firefox postman steam videostream brave-browser-dev google-backup-and-sync keepassx steelseries-exactmouse-tool virtualbox"
+BREW_CASKS="google-chrome keybase rectangles visual-studio-code alfred sublime-text vlc discord google-drive obs docker intellij-idea slack aws-vault spotify firefox steam keepassxc steelseries-exactmouse-tool"
+
+# BREW_CASKS_OLD="abstract chromedriver google-chrome keybase sizeup streamlink-twitch-gui visual-studio-code alfred homebrew/cask-versions/google-chrome-canary logitech-gaming-software sketch sublime-text vlc android-studio discord google-drive-file-stream skyfonts supertuxkart zulu android-studio-preview disk-inventory-x hyper obs skype tor-browser zulu8 docker intellij-idea slack aws-vault osu spotify ubiquiti-unifi-controller balsamiq-mockups firefox postman steam videostream brave-browser-dev google-backup-and-sync keepassx steelseries-exactmouse-tool virtualbox"
 
 # NODE
-NPM_PACKAGES="n ncu"
+# NPM_PACKAGES="n ncu"
 
 # HYPER (installed with hpm)
-HPM_PACKAGES="hyper-snazzy hyperlinks"
+# HPM_PACKAGES="hyper-snazzy hyperlinks"
 
 # BREW (OPTIONAL)
-OPTIONAL_BREW_PACKAGES="python3 wget gpg curl ncdu rsync ack yarn awscli git lolcat nyancat streamlink ruby"
+OPTIONAL_BREW_PACKAGES="wget gpg curl rsync ack awscli git lolcat nyancat"
+# OPTIONAL_BREW_PACKAGES="python3 wget gpg curl ncdu rsync ack yarn awscli git lolcat nyancat streamlink ruby"
 
 # --------------------------------------------------------------
 # DANGER ZONE! EDIT THE FOLLOWING SETTINGS WITH CARE
@@ -23,10 +26,11 @@ OPTIONAL_BREW_PACKAGES="python3 wget gpg curl ncdu rsync ack yarn awscli git lol
 # --------------------------------------------------------------
 
 # BREW (ESSENTIAL)
-ESSENTIAL_BREW_PACKAGES="coreutils diff-so-fancy fish node python pyenv vim bat ripgrep fd lcd"
+ESSENTIAL_BREW_PACKAGES="coreutils diff-so-fancy fish node python pyenv vim bat ripgrep fd lcd sd"
 
 # FISH (installed with fisherman)
 FISHERMAN_PACKAGES="rafaelrinaldi/pure jethrokuan/z jbonjean/re-search evanlucas/fish-kubectl-completions reitzig/sdkman-for-fish"
+# no longer used: FISHERMAN_PACKAGES="jbonjean/re-search"
 
 # BASH (installed with brew)
 BASH_BREW_PACKAGES="bash bash-completion z"
