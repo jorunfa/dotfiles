b52c31349729e4af44616c6cc4b3493fe79c5903
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2023-02-06 13:46:02 +0100

    add dust, duf

--- settings.sh
+++ settings.sh
@@ -6,7 +6,7 @@ GIT_EMAIL="j.b.fagerjord@gmail.com"
 # The applications below are regular apps such as Chrome.
 # Please make sure that you remove all the programs you don't want to install on your computer
 # P.S. The installer script won't install the applications unless you tell it to. It's your call  :)
-BREW_CASKS="google-chrome keybase rectangles visual-studio-code alfred sublime-text vlc discord google-drive obs docker intellij-idea slack aws-vault spotify firefox steam keepassxc steelseries-exactmouse-tool logitech-g-hub"
+BREW_CASKS="google-chrome keybase rectangles visual-studio-code alfred sublime-text vlc discord google-drive obs docker intellij-idea slack aws-vault spotify firefox steam keepassxc steelseries-exactmouse-tool logitech-g-hub dust duf"
 
 # BREW_CASKS_OLD="abstract chromedriver google-chrome keybase sizeup streamlink-twitch-gui visual-studio-code alfred homebrew/cask-versions/google-chrome-canary logitech-gaming-software sketch sublime-text vlc android-studio discord google-drive-file-stream skyfonts supertuxkart zulu android-studio-preview disk-inventory-x hyper obs skype tor-browser zulu8 docker intellij-idea slack aws-vault osu spotify ubiquiti-unifi-controller balsamiq-mockups firefox postman steam videostream brave-browser-dev google-backup-and-sync keepassx steelseries-exactmouse-tool virtualbox"
 
