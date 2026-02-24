d65c436cfb93d7557ac1f5b175ef1800cef822dd
Jørund B. Fagerjord <j.b.fagerjord@gmail.com>
2026-02-24 12:52:13 +0100

    Fix CATALINA_HOME path

--- dotfiles/fish/config/config.fish
+++ dotfiles/fish/config/config.fish
@@ -51,7 +51,7 @@ set -gx PATH '/Applications/IntelliJ IDEA.app/Contents/MacOS' $PATH
 
 # mise
 mise activate fish | source
-set -gx CATALINA_HOME (mise where tomcat 2>/dev/null)
+set -gx CATALINA_HOME (mise where tomcat 2>/dev/null)/apache-tomcat-9.0.52
 
 # bun
 set --export BUN_INSTALL "$HOME/.bun"
